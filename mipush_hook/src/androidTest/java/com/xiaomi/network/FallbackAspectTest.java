a140bc9e01f93fe5fc3c39ba42af12ef4c19455e
Nihility <Nihility.t@gmail.com>
2024-12-20 18:02:03 +0800

    refactor: move FallbackAspectTest to mipush_hook

--- mipush_hook/src/androidTest/java/com/xiaomi/network/FallbackAspectTest.java
+++ mipush_hook/src/androidTest/java/com/xiaomi/network/FallbackAspectTest.java
@@ -2,12 +2,11 @@ package com.xiaomi.network;
 
 import static org.junit.Assert.assertEquals;
 
-import android.content.Context;
-
 import androidx.annotation.NonNull;
 
+import com.nihility.Configurations;
+import com.nihility.Dependencies;
 import com.xiaomi.smack.ConnectionConfiguration;
-import com.xiaomi.xmsf.utils.ConfigCenter;
 
 import org.junit.Test;
 
@@ -18,9 +17,9 @@ public class FallbackAspectTest {
     @Test
     public void useUserDefinedXmppServerHostFirst() {
         String expectedHost = "hooked.host";
-        ConfigCenter.setInstance(new ConfigCenter() {
+        Dependencies.getInstance().init(new Configurations() {
             @Override
-            public String getXMPPServer(Context ctx) {
+            public String getXMPPServer() {
                 return expectedHost;
             }
         });
