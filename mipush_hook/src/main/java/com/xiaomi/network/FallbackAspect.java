18bdae509ac5e4f495bfdda15e5887e1c0b43545
Nihility <Nihility.t@gmail.com>
2024-12-20 16:06:53 +0800

    refactor: move FallbackAspect to mipush_hook

---
+++ mipush_hook/src/main/java/com/xiaomi/network/FallbackAspect.java
@@ -0,0 +1,33 @@
+package com.xiaomi.network;
+
+
+import android.text.TextUtils;
+
+import com.nihility.Configurations;
+import com.nihility.Dependencies;
+import com.xiaomi.smack.ConnectionConfiguration;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+import java.util.ArrayList;
+
+@Aspect
+public class FallbackAspect {
+
+    @Around("execution(* com.xiaomi.network.Fallback.getHosts(..)) && target(fallback) && args(usePort)")
+    public Object useUserDefinedXmppServerHostFirst(final ProceedingJoinPoint joinPoint, Fallback fallback, boolean usePort) throws Throwable {
+        ArrayList<String> hosts = (ArrayList<String>) joinPoint.proceed();
+
+        if (TextUtils.equals(fallback.host, ConnectionConfiguration.getXmppServerHost())) {
+            Configurations configurations = Dependencies.getInstance().configuration();
+            String userHost = configurations.getXMPPServer();
+            if (!TextUtils.isEmpty(userHost)) {
+                hosts.add(0, userHost);
+            }
+        }
+        return hosts;
+    }
+
+}
