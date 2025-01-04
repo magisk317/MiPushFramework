10401e5567f1fbfd4360bcdaa98cf881a7f57efe
Nihility <Nihility.t@gmail.com>
2025-01-04 18:12:31 +0800

    feat: remove MIPushEventProcessor.postProcessMIPushMessage hook

--- push/src/main/java/com/xiaomi/push/service/MIPushEventProcessorAspect.java
+++ push/src/main/java/com/xiaomi/push/service/MIPushEventProcessorAspect.java
@@ -1,15 +1,9 @@
 package com.xiaomi.push.service;
 
 import static com.xiaomi.push.service.MIPushEventProcessor.buildContainer;
-import static com.xiaomi.push.service.MiPushMsgAck.sendAckMessage;
-import static com.xiaomi.push.service.MiPushMsgAck.sendAppNotInstallNotification;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
-import android.text.TextUtils;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -19,14 +13,11 @@ import com.com.xiaomi.channel.commonutils.android.AppInfoUtilsAspect;
 import com.elvishew.xlog.Logger;
 import com.elvishew.xlog.XLog;
 import com.nihility.service.RegistrationRecorder;
-import com.xiaomi.channel.commonutils.android.AppInfoUtils;
 import com.xiaomi.channel.commonutils.reflect.JavaCalls;
 import com.xiaomi.push.service.clientReport.ReportConstants;
-import com.xiaomi.xmpush.thrift.ActionType;
 import com.xiaomi.xmpush.thrift.PushMetaInfo;
 import com.xiaomi.xmpush.thrift.XmPushActionContainer;
 import com.xiaomi.xmpush.thrift.XmPushThriftSerializeUtils;
-import com.xiaomi.xmsf.R;
 import com.xiaomi.xmsf.push.type.TypeFactory;
 import com.xiaomi.xmsf.push.utils.Configurations;
 import com.xiaomi.xmsf.utils.ConvertUtils;
@@ -37,8 +28,6 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
-import top.trumeet.common.cache.ApplicationNameCache;
-import top.trumeet.common.utils.CustomConfiguration;
 import top.trumeet.common.utils.Utils;
 import top.trumeet.mipush.provider.db.EventDb;
 import top.trumeet.mipush.provider.db.RegisteredApplicationDb;
@@ -148,129 +137,6 @@ public class MIPushEventProcessorAspect {
         userAllow(type, pushService);
     }
 
-    @Around("execution(* com.xiaomi.push.service.MIPushEventProcessor.postProcessMIPushMessage(..)) && args(pushService, realTargetPackage, decryptedContent, intent)")
-    public void postProcessMIPushMessage(final ProceedingJoinPoint joinPoint,
-                                         XMPushService pushService, String realTargetPackage, byte[] decryptedContent, Intent intent) {
-        logger.d(joinPoint.getSignature());
-
-        XmPushActionContainer container = buildContainer(decryptedContent);
-        PushMetaInfo metaInfo = container.getMetaInfo();
-
-        boolean isBusinessMessage = MIPushNotificationHelper.isBusinessMessage(container);
-        boolean pkgInstalled = AppInfoUtils.isPkgInstalled(pushService, container.packageName);
-
-        if (JavaCalls.<Boolean>callStaticMethod(MIPushEventProcessor.class.getName(), "isMIUIOldAdsSDKMessage", container) &&
-                JavaCalls.<Boolean>callStaticMethod(MIPushEventProcessor.class.getName(), "isMIUIPushSupported", pushService, realTargetPackage)) {
-            JavaCalls.callStaticMethod(MIPushEventProcessor.class.getName(), "sendMIUIOldAdsAckMessage", pushService, container);
-        } else if (JavaCalls.<Boolean>callStaticMethod(MIPushEventProcessor.class.getName(), "isMIUIPushMessage", container) &&
-                !JavaCalls.<Boolean>callStaticMethod(MIPushEventProcessor.class.getName(), "isMIUIPushSupported", pushService, realTargetPackage) &&
-                !JavaCalls.<Boolean>callStaticMethod(MIPushEventProcessor.class.getName(), "predefinedNotification", container)) {
-            JavaCalls.callStaticMethod(MIPushEventProcessor.class.getName(), "sendMIUINewAdsAckMessage", pushService, container);
-        } else {
-            if (!pkgInstalled) {
-                sendAppNotInstallNotification(pushService, container);
-                return;
-            }
-            if (ActionType.Registration == container.getAction()) {
-                intent.putExtra(ReportConstants.EVENT_MESSAGE_TYPE, ReportConstants.REGISTER_TYPE);
-                String pkgName = container.getPackageName();
-                SharedPreferences sp = pushService.getSharedPreferences(PushServiceConstants.PREF_KEY_REGISTERED_PKGS, 0);
-                SharedPreferences.Editor editor = sp.edit();
-                editor.putString(pkgName, container.appid);
-                editor.commit();
-                MIPushAppInfo.getInstance(pushService).removeDisablePushPkg(pkgName);
-                MIPushAppInfo.getInstance(pushService).removeDisablePushPkgCache(pkgName);
-
-                RegistrationRecorder.getInstance().initContext(pushService);
-                RegistrationRecorder.getInstance().recordRegSec(container);
-            }
-
-            // todo: delete tracking code {
-            if (!TextUtils.isEmpty(metaInfo.getId())) {
-                intent.putExtra("messageId", metaInfo.getId());
-            }
-            if (MIPushNotificationHelper.isNormalNotificationMessage(container)) {
-                intent.putExtra(ReportConstants.EVENT_MESSAGE_TYPE, 1000);
-            }
-            if (MIPushNotificationHelper.isPassThoughMessage(container)) {
-                intent.putExtra(ReportConstants.EVENT_MESSAGE_TYPE, 2000);
-            }
-            if (MIPushNotificationHelper.isBusinessMessage(container)) {
-                intent.putExtra(ReportConstants.EVENT_MESSAGE_TYPE, ReportConstants.AWAKE_TYPE);
-            }
-            // }
-
-            String title;
-            String description;
-            if (metaInfo != null) {
-                title = metaInfo.getTitle();
-                description = metaInfo.getDescription();
-
-                completeTitleOrDescriptionIfExistsAnyone(pushService, realTargetPackage, title, description, metaInfo);
-            }
-
-            RegisteredApplication application = RegisteredApplicationDb.registerApplication(
-                    realTargetPackage, false);
-            XmPushActionContainer decorated = decoratedContainer(realTargetPackage, container);
-            CustomConfiguration configuration = new CustomConfiguration(decorated.metaInfo.getExtra());
-            boolean awake = configuration.get(PushConstants.EXTRA_PARAM_AWAKE, false);
-            boolean isSystemApp = isSystemApp(pushService, realTargetPackage);
-            if (metaInfo == null || TextUtils.isEmpty(metaInfo.getTitle()) || TextUtils.isEmpty(metaInfo.getDescription()) ||
-                    decorated.metaInfo.passThrough == 1 /* ||
-                    (!MIPushNotificationHelper.isNotifyForeground(metaInfo.getExtra()) && MIPushNotificationHelper.isApplicationForeground(pushService, container.packageName)) */) {
-                if (PushConstants.PUSH_SERVICE_PACKAGE_NAME.contains(container.packageName) &&
-                        !container.isEncryptAction() && metaInfo != null && metaInfo.getExtra() != null &&
-                        metaInfo.getExtra().containsKey("ab")) {
-                    sendAckMessage(pushService, container);
-                    logger.v("receive abtest message. ack it." + metaInfo.getId());
-                } else if (awake || container.action != ActionType.SendMessage || isSystemApp) {
-                    pushService.sendBroadcast(intent, MIPushHelper.getReceiverPermission(container.packageName));
-                }
-            } else {
-                String key = null;
-                if (metaInfo.extra != null) {
-                    key = metaInfo.extra.get("jobkey");
-                }
-                if (TextUtils.isEmpty(key)) {
-                    key = metaInfo.getId();
-                }
-                boolean isDupMessage = MiPushMessageDuplicate.isDuplicateMessage(pushService, container.packageName, key);
-                if (isDupMessage && !isMockMessage(container)) {
-                    logger.w("drop a duplicate message, key=" + key);
-                } else {
-                    MyMIPushNotificationHelper.notifyPushMessage(pushService, decryptedContent);
-
-                    if (awake || isSystemApp) {
-                        if (decorated.metaInfo.passThrough == 1) {
-                            pushService.sendBroadcast(intent, MIPushHelper.getReceiverPermission(container.packageName));
-                        } else if (!isBusinessMessage) {
-                            Intent messageArrivedIntent = new Intent(PushConstants.MIPUSH_ACTION_MESSAGE_ARRIVED);
-                            messageArrivedIntent.putExtra(PushConstants.MIPUSH_EXTRA_PAYLOAD, decryptedContent);
-                            messageArrivedIntent.setPackage(container.packageName);
-                            pushService.sendBroadcast(messageArrivedIntent, MIPushHelper.getReceiverPermission(container.packageName));
-                        }
-                    }
-                }
-                sendAckMessage(pushService, container);
-            }
-            if (container.getAction() == ActionType.UnRegistration && !PushConstants.PUSH_SERVICE_PACKAGE_NAME.equals(pushService.getPackageName())) {
-                pushService.stopSelf();
-            }
-        }
-    }
-
-    private static boolean isSystemApp(XMPushService pushService, String realTargetPackage) {
-        boolean isSystemApp = false;
-        try {
-            int flags = pushService.getPackageManager().getApplicationInfo(realTargetPackage, 0).flags &
-                    (ApplicationInfo.FLAG_SYSTEM | ApplicationInfo.FLAG_UPDATED_SYSTEM_APP);
-            isSystemApp = flags != 0;
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-        return isSystemApp;
-    }
-
     private static XmPushActionContainer decoratedContainer(String realTargetPackage, XmPushActionContainer container) {
         XmPushActionContainer decorated = container.deepCopy();
         try {
@@ -279,19 +145,6 @@ public class MIPushEventProcessorAspect {
             // Ignore
         }
         return decorated;
-    }
-
-    private static void completeTitleOrDescriptionIfExistsAnyone(XMPushService pushService, String realTargetPackage, String title, String description, PushMetaInfo metaInfo) {
-        if (!TextUtils.isEmpty(title) || !TextUtils.isEmpty(description)) {
-            if (TextUtils.isEmpty(title)) {
-                CharSequence appName = ApplicationNameCache.getInstance().getAppName(pushService, realTargetPackage);
-                metaInfo.setTitle(appName.toString());
-            }
-
-            if (TextUtils.isEmpty(description)) {
-                metaInfo.setDescription(pushService.getString(R.string.see_pass_though_msg));
-            }
-        }
     }
 
 }
