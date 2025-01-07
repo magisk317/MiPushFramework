08c37604a3b16a5e87454c4bb119362c4c0275c1
Nihility <Nihility.t@gmail.com>
2025-01-07 20:27:58 +0800

    refactor: Make UsageStatsPermissionActivity inherit from RequestPermissionActivity.

--- push/src/main/java/top/trumeet/mipushframework/wizard/UsageStatsPermissionActivity.java
+++ push/src/main/java/top/trumeet/mipushframework/wizard/UsageStatsPermissionActivity.java
@@ -1,94 +1,39 @@
 package top.trumeet.mipushframework.wizard;
 
-import android.content.Intent;
+import android.app.Activity;
 import android.os.Build;
-import android.os.Bundle;
-import android.text.Html;
 
-import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
 
-import com.android.setupwizardlib.view.NavigationBar;
 import com.xiaomi.xmsf.R;
 
-import top.trumeet.mipushframework.utils.PermissionUtils;
+public class UsageStatsPermissionActivity extends RequestPermissionActivity {
 
-/**
- * Created by Trumeet on 2017/8/25.
- *
- * @author Trumeet
- */
-
-public class UsageStatsPermissionActivity extends PushControllerWizardActivity implements NavigationBar.NavigationBarListener {
-    private final UsageStatsPermissionOperator permissionOperator = new UsageStatsPermissionOperator(this);
-    private boolean nextClicked = false;
-
+    @NonNull
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            nextPage();
-            finish();
-            return;
-        }
-        connect();
+    protected PermissionOperator getPermissionOperator() {
+        return new UsageStatsPermissionOperator(this);
     }
 
+    @NonNull
     @Override
-    public void onResume() {
-        super.onResume();
-
-        if (permissionOperator.isPermissionGranted()) {
-            nextPage();
-            finish();
-        } else if (nextClicked) {
-            layout.getNavigationBar()
-                    .getNextButton()
-                    .setText(R.string.retry);
-        }
+    protected String getPermissionTitle() {
+        return getString(R.string.wizard_title_stats_permission);
     }
 
+    @NonNull
     @Override
-    public void onConnected(Bundle savedInstanceState) {
-        super.onConnected(savedInstanceState);
-
-        if (permissionOperator.isPermissionGranted()) {
-            nextPage();
-            finish();
-            return;
-        }
-        layout.getNavigationBar()
-                .setNavigationBarListener(this);
-        mText.setText(Html.fromHtml(getString(R.string.wizard_title_stats_permission_text)));
-        layout.setHeaderText(Html.fromHtml(getString(R.string.wizard_title_stats_permission)));
-        setContentView(layout);
+    public String getPermissionDescription() {
+        return getString(R.string.wizard_title_stats_permission_text);
     }
 
+    @NonNull
     @Override
-    public void onNavigateBack() {
-        onBackPressed();
-    }
-
-    @Override
-    public void onNavigateNext() {
-        nextClicked = true;
-        if (!permissionOperator.isPermissionGranted()) {
-            if (PermissionUtils.canAssignPermissionViaAppOps()) {
-                permissionOperator.requestPermissionSilently();
-            } else {
-                permissionOperator.requestPermission();
-            }
-        } else {
-            nextPage();
-        }
-    }
-
-    private void nextPage() {
+    protected Class<? extends Activity> nextPageClass() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            startActivity(new Intent(this,
-                    AlertWindowPermissionActivity.class));
+            return AlertWindowPermissionActivity.class;
         } else {
-            startActivity(new Intent(this,
-                    FinishWizardActivity.class));
+            return FinishWizardActivity.class;
         }
     }
 
