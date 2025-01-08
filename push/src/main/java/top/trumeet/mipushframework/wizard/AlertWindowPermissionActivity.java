66e113157eab0335b542085246aa52217e37639b
Nihility <Nihility.t@gmail.com>
2025-01-08 17:41:34 +0800

    refactor: extract class AlertWindowPermissionInfo from class AlertWindowPermissionActivity

--- push/src/main/java/top/trumeet/mipushframework/wizard/AlertWindowPermissionActivity.java
+++ push/src/main/java/top/trumeet/mipushframework/wizard/AlertWindowPermissionActivity.java
@@ -6,32 +6,32 @@ import android.os.Build;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 
-import com.xiaomi.xmsf.R;
-
 @RequiresApi(api = Build.VERSION_CODES.M)
 public class AlertWindowPermissionActivity extends RequestPermissionActivity {
+
+    private final AlertWindowPermissionInfo alertWindowPermissionInfo = new AlertWindowPermissionInfo(this);
 
     @NonNull
     @Override
     protected PermissionOperator getPermissionOperator() {
-        return new AlertWindowPermissionOperator(this);
+        return alertWindowPermissionInfo.getPermissionOperator();
     }
 
     @Override
     @NonNull
     protected Class<? extends Activity> nextPageClass() {
-        return FinishWizardActivity.class;
+        return alertWindowPermissionInfo.nextPageClass();
     }
 
     @Override
     @NonNull
     protected String getPermissionTitle() {
-        return getString(R.string.wizard_title_alert_window_permission);
+        return alertWindowPermissionInfo.getPermissionTitle();
     }
 
     @Override
     @NonNull
     public String getPermissionDescription() {
-        return getString(R.string.wizard_title_alert_window_text);
+        return alertWindowPermissionInfo.getPermissionDescription();
     }
 }
---
+++ push/src/main/java/top/trumeet/mipushframework/wizard/AlertWindowPermissionInfo.java
@@ -0,0 +1,43 @@
+package top.trumeet.mipushframework.wizard;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+
+import com.xiaomi.xmsf.R;
+
+@RequiresApi(api = Build.VERSION_CODES.M)
+public class AlertWindowPermissionInfo implements PermissionInfo {
+    private final Context context;
+
+    public AlertWindowPermissionInfo(Context context) {
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public PermissionOperator getPermissionOperator() {
+        return new AlertWindowPermissionOperator(context);
+    }
+
+    @Override
+    @NonNull
+    public Class<? extends Activity> nextPageClass() {
+        return FinishWizardActivity.class;
+    }
+
+    @Override
+    @NonNull
+    public String getPermissionTitle() {
+        return context.getString(R.string.wizard_title_alert_window_permission);
+    }
+
+    @Override
+    @NonNull
+    public String getPermissionDescription() {
+        return context.getString(R.string.wizard_title_alert_window_text);
+    }
+}
