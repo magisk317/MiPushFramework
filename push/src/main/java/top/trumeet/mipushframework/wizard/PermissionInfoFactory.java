24f951484ddcce9c2cec29a71f11184f0d782c44
Nihility <Nihility.t@gmail.com>
2025-01-08 17:39:51 +0800

    refactor: use Class.forName to locate subclass of PermissionInfo

--- push/src/main/java/top/trumeet/mipushframework/wizard/PermissionInfoFactory.java
+++ push/src/main/java/top/trumeet/mipushframework/wizard/PermissionInfoFactory.java
@@ -3,19 +3,25 @@ package top.trumeet.mipushframework.wizard;
 import android.content.Context;
 import android.content.Intent;
 
+import java.lang.reflect.Constructor;
+
 public class PermissionInfoFactory {
     private static final String intentKey = PermissionInfo.class.getSimpleName();
 
     public static PermissionInfo create(String className, Context context) {
-        if (UsageStatsPermissionInfo.class.getSimpleName().equals(className)) {
-            return new UsageStatsPermissionInfo(context);
+        try {
+            Class<?> clazz = Class.forName(className);
+            Constructor<?> constructor = clazz.getConstructor(Context.class);
+            return (PermissionInfo) constructor.newInstance(context);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return null;
     }
 
     public static Intent bindPermissionInfo(
             Intent intent, Class<? extends PermissionInfo> permissionInfoClass) {
-        return intent.putExtra(intentKey, permissionInfoClass.getSimpleName());
+        return intent.putExtra(intentKey, permissionInfoClass.getCanonicalName());
     }
 
     public static PermissionInfo createFrom(Intent intent, Context context) {
