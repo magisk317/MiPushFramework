228d37bc06f367550eceb2ce157e2165d0fb72b2
Nihility <Nihility.t@gmail.com>
2025-01-10 21:43:13 +0800

    feat: change debug log range

--- push/src/main/java/com/xiaomi/push/service/LogDebugAspect.java
+++ push/src/main/java/com/xiaomi/push/service/LogDebugAspect.java
@@ -1,5 +1,7 @@
 package com.xiaomi.push.service;
 
+import androidx.annotation.NonNull;
+
 import com.elvishew.xlog.Logger;
 import com.elvishew.xlog.XLog;
 import com.xiaomi.xmpush.thrift.XmPushActionContainer;
@@ -16,7 +18,7 @@ import java.util.Collections;
 public class LogDebugAspect {
     private static final String TAG = LogDebugAspect.class.getSimpleName();
     private static final Logger logger = XLog.tag(TAG).build();
-    private static final ThreadLocal<Integer> level = new ThreadLocal<Integer>() {
+    private static final ThreadLocal<Integer> indentLevel = new ThreadLocal<Integer>() {
         @Override
         protected Integer initialValue() {
             return 0;
@@ -24,43 +26,65 @@ public class LogDebugAspect {
     };
 
 
-    @Around("   !call(* *..DebugAspect.*(..))" +
-            "&& !call(* com.xiaomi.channel.commonutils.reflect..*(..))" +
-            "&& !call(* com.xiaomi.channel.commonutils.logger..*(..))" +
-            "&& !call(* com.xiaomi.push.service.JobScheduler..*(..))" +
-            "&& !call(* com.xiaomi.push.service.RC4Cryption..*(..))" +
-            "&& (" +
-            "   call(* com.xiaomi.mipush..*(..))" +
-            "|| call(* com.xiaomi.push..*(..))" +
-            "|| call(* com.xiaomi.channel..*(..))" +
-            "|| execution(* com.xiaomi.push.service.XMPushService$2.handleMessage(..))" +
-            ")")
+    @Around("(execution(* com.xiaomi.push.service.XMPushService*.*(..))" +
+            " || execution(* com.xiaomi.push.service.PacketSync*.*(..))" +
+            " || execution(* com.xiaomi.push.service.ClientEventDispatcher*.*(..))" +
+            " || execution(* com.xiaomi.push.service.MIPushEventProcessor*.*(..))" +
+            " || execution(* com.xiaomi.push.service.MIPushNotificationHelper*.*(..))" +
+            " || execution(* com.xiaomi.push.service.NotificationManagerHelper*.*(..))" +
+            ") && !within(is(FinalType))")
     public Object logger(final ProceedingJoinPoint joinPoint) throws Throwable {
         if (!ConfigCenter.getInstance().isDebugMode()) {
             return joinPoint.proceed();
         }
 
-        int curLevel = level.get();
-        String prefix =String.join("", Collections.nCopies(curLevel, "|\t"));
-        if (joinPoint.getThis() != null) {
-            prefix += joinPoint.getThis().getClass().getSimpleName() + " " + joinPoint.toLongString();
-        } else {
-            prefix += joinPoint.toLongString();
-        }
+        String prefix = getPrefix(joinPoint, indentLevel.get());
         logger.d(prefix);
-        level.set(curLevel + 1);
+
+        increaseIndent();
         Object ret;
         try {
             ret = joinPoint.proceed();
+        } catch (Throwable e) {
+            logThrowable(e, prefix);
+            throw e;
         } finally {
-            level.set(curLevel);
+            decreaseIndent();
         }
+
+        logResult(ret, prefix);
+        return ret;
+    }
+
+    private static void logThrowable(Throwable e, String prefix) {
+        logger.d(prefix + " -> [" + e + "]");
+    }
+
+    private static void logResult(Object ret, String prefix) {
         if (ret instanceof XmPushActionContainer) {
             logger.d(prefix + " -> [" + ConvertUtils.toJson((XmPushActionContainer) ret) + "]");
         } else {
             logger.d(prefix + " -> [" + ret + "]");
         }
-        return ret;
+    }
+
+    private static void decreaseIndent() {
+        indentLevel.set(indentLevel.get() - 1);
+    }
+
+    private static void increaseIndent() {
+        indentLevel.set(indentLevel.get() + 1);
+    }
+
+    @NonNull
+    private static String getPrefix(ProceedingJoinPoint joinPoint, int curLevel) {
+        String prefix = String.join("", Collections.nCopies(curLevel, "|\t"));
+        if (joinPoint.getThis() != null) {
+            prefix += joinPoint.getThis().getClass().getSimpleName() + " " + joinPoint.toLongString();
+        } else {
+            prefix += joinPoint.toLongString();
+        }
+        return prefix;
     }
 
 }
