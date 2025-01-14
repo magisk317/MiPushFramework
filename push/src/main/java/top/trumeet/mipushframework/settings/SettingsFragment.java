950e1254d25a4ff9a3e416c12b9c2f5c8288cbbf
Nihility <Nihility.t@gmail.com>
2025-01-14 16:57:58 +0800

    refactor: extract methods

--- push/src/main/java/top/trumeet/mipushframework/settings/SettingsFragment.java
+++ push/src/main/java/top/trumeet/mipushframework/settings/SettingsFragment.java
@@ -3,6 +3,7 @@ package top.trumeet.mipushframework.settings;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.Uri;
@@ -14,6 +15,7 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 
 import com.android.settings.widget.EntityHeaderController;
@@ -29,8 +31,6 @@ import com.xiaomi.xmsf.utils.ConfigCenter;
 
 import moe.shizuku.preference.Preference;
 import moe.shizuku.preference.PreferenceFragment;
-import moe.shizuku.preference.PreferenceGroup;
-import moe.shizuku.preference.SwitchPreferenceCompat;
 import top.trumeet.common.Constants;
 import top.trumeet.mipush.provider.register.RegisteredApplication;
 import top.trumeet.mipushframework.MainActivity;
@@ -67,6 +67,10 @@ public class SettingsFragment extends PreferenceFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        updateConnectionStatus();
+    }
+
+    private void updateConnectionStatus() {
         messenger.send(new Intent(XMPushServiceMessenger.IntentGetConnectionStatus));
     }
 
@@ -74,48 +78,41 @@ public class SettingsFragment extends PreferenceFragment {
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.settings);
         setPreferenceOnclick("key_get_log", preference -> {
-            startActivity(new Intent()
-                    .setComponent(new ComponentName(Constants.SERVICE_APP_NAME,
-                            Constants.SHARE_LOG_COMPONENT_NAME)));
+            shareLogs();
             return true;
         });
         {
+            Uri treeUri = getConfigurationDirectory();
             Preference preference = getPreference("configuration_directory");
-            Uri treeUri = ConfigCenter.getInstance().getConfigurationDirectory(getActivity());
             if (treeUri != null) {
                 preference.setSummary(treeUri.toString());
             }
             preference.setOnPreferenceClickListener(pref -> {
-                openDirectory(Uri.fromFile(
-                        getActivity().getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)));
+                openDirectory();
                 return true;
             });
         }
         {
+            String host = getXMPPServer();
             Preference preference = getPreference("XMPP_server");
-            String host = ConfigCenter.getInstance().getXMPPServer(getActivity());
             if (!TextUtils.isEmpty(host)) {
                 preference.setSummary(host);
             }
             preference.setOnPreferenceClickListener(pref -> {
-                EditText editText = new EditText(getActivity());
-                editText.setHint(ConnectionConfiguration.getXmppServerHost() + ":" + PushServiceConstants.XMPP_SERVER_PORT);
-                editText.setText(ConfigCenter.getInstance().getXMPPServer(getActivity()));
-                editText.setSingleLine();
+                EditText editText = getXMPPServerAddrEditText();
                 AlertDialog.Builder build = new AlertDialog.Builder(getActivity())
                         .setView(editText)
                         .setTitle(R.string.settings_XMPP_server)
                         .setCancelable(true)
                         .setPositiveButton(android.R.string.ok, (dialogInterface, i) -> {
                             String newHost = String.valueOf(editText.getText()).trim();
-                            ConfigCenter.getInstance().setXMPPServer(getActivity(), newHost);
+                            setXMPPServer(newHost);
                             if (TextUtils.isEmpty(newHost)) {
                                 preference.setSummary(R.string.settings_XMPP_server_summary);
                             } else {
                                 preference.setSummary(newHost);
                             }
-                            new InternalMessenger(getContext()).send(
-                                    new Intent(PushConstants.ACTION_RESET_CONNECTION));
+                            sendXMPPReconnectRequest();
                         });
                 build.create().show();
                 return true;
@@ -124,11 +121,7 @@ public class SettingsFragment extends PreferenceFragment {
         {
             Preference preference = new Preference(getContext());
             preference.setOnPreferenceClickListener(pref -> {
-                RegisteredApplicationFragment.MiPushApplications miPushApplications =
-                        RegisteredApplicationFragment.getMiPushApplications();
-                for (RegisteredApplication registeredApplication : miPushApplications.res) {
-                    EntityHeaderController.tryForceRegister(registeredApplication.getPackageName());
-                }
+                tryForceRegisterAllApplications();
                 return true;
             });
             preference.setTitle(getString(R.string.try_to_force_register_all_applications));
@@ -136,16 +129,52 @@ public class SettingsFragment extends PreferenceFragment {
         }
     }
 
-    private void addItem(boolean value, Preference.OnPreferenceChangeListener listener,
-                         CharSequence title, CharSequence summary, PreferenceGroup parent) {
-        SwitchPreferenceCompat preference = new SwitchPreferenceCompat(getContext(),
-                null, moe.shizuku.preference.R.attr.switchPreferenceStyle,
-                R.style.Preference_SwitchPreferenceCompat);
-        preference.setOnPreferenceChangeListener(listener);
-        preference.setTitle(title);
-        preference.setSummary(summary);
-        preference.setChecked(value);
-        parent.addPreference(preference);
+    private static void tryForceRegisterAllApplications() {
+        RegisteredApplicationFragment.MiPushApplications miPushApplications =
+                RegisteredApplicationFragment.getMiPushApplications();
+        for (RegisteredApplication registeredApplication : miPushApplications.res) {
+            EntityHeaderController.tryForceRegister(registeredApplication.getPackageName());
+        }
+    }
+
+    private void sendXMPPReconnectRequest() {
+        new InternalMessenger(getContext()).send(
+                new Intent(PushConstants.ACTION_RESET_CONNECTION));
+    }
+
+    private void setXMPPServer(String newHost) {
+        ConfigCenter.getInstance().setXMPPServer(getActivity(), newHost);
+    }
+
+    private @NonNull EditText getXMPPServerAddrEditText() {
+        EditText editText = new EditText(getActivity());
+        editText.setHint(getXMPPServerHint());
+        editText.setText(getXMPPServer());
+        editText.setSingleLine();
+        return editText;
+    }
+
+    private static @NonNull String getXMPPServerHint() {
+        return ConnectionConfiguration.getXmppServerHost() + ":" + PushServiceConstants.XMPP_SERVER_PORT;
+    }
+
+    private String getXMPPServer() {
+        return ConfigCenter.getInstance().getXMPPServer(getActivity());
+    }
+
+    private Uri getConfigurationDirectory() {
+        return ConfigCenter.getInstance().getConfigurationDirectory(getActivity());
+    }
+
+    private void openDirectory() {
+        openDirectory(Uri.fromFile(
+                getActivity().getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)));
+    }
+
+    private void shareLogs() {
+        startActivity(new Intent()
+                .setComponent(new ComponentName(Constants.SERVICE_APP_NAME,
+                        Constants.SHARE_LOG_COMPONENT_NAME)));
     }
 
     private void setPreferenceOnclick(String key, Preference.OnPreferenceClickListener onPreferenceClickListener) {
@@ -182,19 +211,24 @@ public class SettingsFragment extends PreferenceFragment {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 123 && resultCode == Activity.RESULT_OK && data != null) {
-            Uri uri = data.getData();
-            final int takeFlags = data.getFlags()
-                    & (Intent.FLAG_GRANT_READ_URI_PERMISSION
-                    | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
             try {
-                getContext().getContentResolver().takePersistableUriPermission(uri, takeFlags);
+                Uri uri = saveConfigurationUri(getContext(), data);
                 Preference preference = getPreference("configuration_directory");
                 preference.setSummary(uri.toString());
-                ConfigCenter.getInstance().setConfigurationDirectory(getContext(), uri);
-                ConfigCenter.getInstance().loadConfigurations(getActivity());
             } catch (Throwable e) {
                 logger.e("onActivityResult configuration", e);
             }
         }
+    }
+
+    private @NonNull Uri saveConfigurationUri(Context context, Intent data) {
+        Uri uri = data.getData();
+        final int takeFlags = data.getFlags()
+                & (Intent.FLAG_GRANT_READ_URI_PERMISSION
+                | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        context.getContentResolver().takePersistableUriPermission(uri, takeFlags);
+        ConfigCenter.getInstance().setConfigurationDirectory(context, uri);
+        ConfigCenter.getInstance().loadConfigurations(getActivity());
+        return uri;
     }
 }
