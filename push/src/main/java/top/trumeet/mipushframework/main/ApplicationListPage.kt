d13fd97704a4ecd69448b2a8d0d60058de9e5d6d
Nihility <Nihility.t@gmail.com>
2025-01-21 00:33:23 +0800

    perf: optimize application list rendering performance

--- push/src/main/java/top/trumeet/mipushframework/main/ApplicationListPage.kt
+++ push/src/main/java/top/trumeet/mipushframework/main/ApplicationListPage.kt
@@ -28,6 +28,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -36,12 +38,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalInspectionMode
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
@@ -53,7 +57,9 @@ import com.elvishew.xlog.XLog
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import com.xiaomi.xmsf.R
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import top.trumeet.common.utils.Utils
 import top.trumeet.mipush.provider.register.RegisteredApplication
 import top.trumeet.mipushframework.event.EventItemBinder
@@ -62,6 +68,7 @@ import top.trumeet.ui.theme.Theme
 import java.util.Locale
 
 class ApplicationListPage : Fragment() {
+    private lateinit var defaultAppIcon: BitmapPainter
     private var query by mutableStateOf("")
 
     override fun onCreateView(
@@ -73,8 +80,7 @@ class ApplicationListPage : Fragment() {
         return ComposeView(requireContext()).apply {
             setContent {
                 ApplicationList {
-                    val miPushApplications =
-                        ApplicationPageOperation.getMiPushApplicationsThatQueryMatched(query)
+                    val miPushApplications = ApplicationPageOperation.getMiPushApplicationsThatQueryMatched(query)
                     ApplicationPageOperation.updateRegisteredApplicationDb(context, miPushApplications.res)
                     miPushApplications
                 }
@@ -112,252 +118,289 @@ class ApplicationListPage : Fragment() {
             }
         })
     }
-}
 
-val ErrorColor = Color(0xFFF41804)
-val GreenColor = Color(0xff4caf50)
-val YellowColor = Color(0xffff9800)
+    val ErrorColor = Color(0xFFF41804)
+    val GreenColor = Color(0xff4caf50)
+    val YellowColor = Color(0xffff9800)
 
-@Composable
-fun getRegistrationState(app: RegisteredApplication): Pair<String, Color> {
-    val prefix =
-        if (!app.existServices) stringResource(R.string.mipush_services_not_found) + " - "
-        else ""
-    val color = getRegistrationStateColor(app)
-    return when (app.registeredType) {
-        RegisteredApplication.RegisteredType.Registered -> {
-            Pair(prefix + stringResource(R.string.app_registered), color)
-        }
+    @Composable
+    fun getRegistrationState(app: RegisteredApplication): Pair<String, Color> {
+        val prefix =
+            if (!app.existServices) stringResource(R.string.mipush_services_not_found) + " - "
+            else ""
+        val color = getRegistrationStateColor(app)
+        return when (app.registeredType) {
+            RegisteredApplication.RegisteredType.Registered -> {
+                Pair(prefix + stringResource(R.string.app_registered), color)
+            }
 
-        RegisteredApplication.RegisteredType.Unregistered -> {
-            Pair(prefix + stringResource(R.string.app_registered_error), color)
-        }
-
-//      RegisteredApplication.RegisteredType.NotRegistered
-        else -> {
-            Pair(prefix + stringResource(R.string.status_app_not_registered), color)
-        }
-    }
-}
-
-fun getRegistrationStateColor(app: RegisteredApplication): Color {
-    return if (!app.existServices) ErrorColor
-    else when (app.registeredType) {
-        RegisteredApplication.RegisteredType.Registered -> {
-            GreenColor
-        }
-
-        RegisteredApplication.RegisteredType.Unregistered -> {
-            YellowColor
-        }
+            RegisteredApplication.RegisteredType.Unregistered -> {
+                Pair(prefix + stringResource(R.string.app_registered_error), color)
+            }
 
 //      RegisteredApplication.RegisteredType.NotRegistered
-        else -> {
-            Color.Unspecified
+            else -> {
+                Pair(prefix + stringResource(R.string.status_app_not_registered), color)
+            }
         }
     }
-}
 
-@Composable
-fun ApplicationList(getMiPushApplications: () -> ApplicationPageOperation.MiPushApplications) {
-    var items = getMiPushApplications()
-    var isRefreshing by remember { mutableStateOf(false) }
-    val notUseMiPushCount = items.totalPkg - items.res.size
+    fun getRegistrationStateColor(app: RegisteredApplication): Color {
+        return if (!app.existServices) ErrorColor
+        else when (app.registeredType) {
+            RegisteredApplication.RegisteredType.Registered -> {
+                GreenColor
+            }
 
-    val refreshScope = rememberCoroutineScope()
+            RegisteredApplication.RegisteredType.Unregistered -> {
+                YellowColor
+            }
 
-    Theme {
-        Surface {
-            SwipeRefresh(
-                state = rememberSwipeRefreshState(isRefreshing),
-                onRefresh = {
-                    refreshScope.launch {
-                        isRefreshing = true
-                        items = getMiPushApplications()
-                        isRefreshing = false
-                    }
-                }
-            ) {
-                LazyColumn(Modifier.fillMaxSize()) {
-                    items(items.res, { it.packageName }) {
-                        ApplicationItem(it)
-                    }
-                    item {
-                        Footer(notUseMiPushCount)
+//      RegisteredApplication.RegisteredType.NotRegistered
+            else -> {
+                Color.Unspecified
+            }
+        }
+    }
+
+    @Composable
+    fun ApplicationList(getMiPushApplications: () -> ApplicationPageOperation.MiPushApplications) {
+        val context = LocalContext.current
+        val isPreview = LocalInspectionMode.current
+        var items by remember {
+            mutableStateOf(
+                if (isPreview) getMiPushApplications()
+                else ApplicationPageOperation.MiPushApplications()
+            )
+        }
+        val notUseMiPushCount by remember { derivedStateOf { items.totalPkg - items.res.size } }
+        var isRefreshing by remember { mutableStateOf(false) }
+
+        val refreshScope = rememberCoroutineScope { Dispatchers.IO }
+        val onRefresh: () -> Unit = {
+            isRefreshing = true
+            refreshScope.launch {
+                items = getMiPushApplications()
+                isRefreshing = false
+                items.res.forEach { cacheIcon(context, it) }
+            }
+        }
+        LaunchedEffect(Unit) { onRefresh() }
+
+        Theme {
+            Surface {
+                SwipeRefresh(
+                    state = rememberSwipeRefreshState(isRefreshing),
+                    onRefresh = onRefresh
+                ) {
+                    LazyColumn(Modifier.fillMaxSize()) {
+                        items(items.res, { it.packageName }) {
+                            ApplicationItem(it)
+                        }
+                        item {
+                            Footer(notUseMiPushCount)
+                        }
                     }
                 }
             }
         }
     }
-}
 
-@Composable
-private fun Footer(notUseMiPushCount: Int) {
-    val context = LocalContext.current
-    Row(verticalAlignment = Alignment.CenterVertically) {
-        Icon(
-            painterResource(R.drawable.ic_info_outline_black_24dp),
-            null,
-            tint = Color(0xFF757575),
-            modifier = Modifier.padding(10.dp)
-        )
-        Text(
-            ApplicationPageOperation.getNotSupportHint(
-                context,
-                notUseMiPushCount
+    @Composable
+    private fun Footer(notUseMiPushCount: Int) {
+        val context = LocalContext.current
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Icon(
+                painterResource(R.drawable.ic_info_outline_black_24dp),
+                null,
+                tint = Color(0xFF757575),
+                modifier = Modifier.padding(10.dp)
             )
-        )
-    }
-}
-
-@Composable
-private fun ApplicationItem(item: RegisteredApplication) {
-    val context = LocalContext.current
-    val icon = getIconFor(item)
-    val registrationState = getRegistrationState(item)
-
-    Row(
-        Modifier
-            .clickable { EventItemBinder.startManagePermissions(context, item.packageName, true) }
-            .padding(10.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Image(icon, item.appName, modifier = Modifier.size(48.dp))
-        Spacer(Modifier.width(20.dp))
-        Column {
-            AppInfo(item, registrationState)
-            LastReceive(item)
+            Text(
+                ApplicationPageOperation.getNotSupportHint(
+                    context,
+                    notUseMiPushCount
+                )
+            )
         }
     }
-}
 
-@Composable
-private fun LastReceive(item: RegisteredApplication) {
-    val context = LocalContext.current
-    Text(
-        if (item.lastReceiveTime.time == 0L) ""
-        else stringResource(R.string.last_receive) + ParseUtils.getFriendlyDateString(
-            item.lastReceiveTime,
-            Utils.getUTC(),
-            context
-        ),
-        style = MaterialTheme.typography.bodyLarge,
-    )
-}
+    @Composable
+    private fun ApplicationItem(item: RegisteredApplication) {
+        val context = LocalContext.current
+        val isPreview = LocalInspectionMode.current
+        var icon by remember { mutableStateOf(getIconFromCache(context, isPreview, item)) }
+        val registrationState = getRegistrationState(item)
 
-@Composable
-private fun AppInfo(
-    item: RegisteredApplication,
-    registrationState: Pair<String, Color>
-) {
-    Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+        LaunchedEffect(Unit) {
+            withContext(Dispatchers.IO) {
+                cacheIcon(context, item)?.let { icon = it }
+            }
+        }
+
+        Row(
+            Modifier
+                .clickable { EventItemBinder.startManagePermissions(context, item.packageName, true) }
+                .padding(10.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(icon, item.appName, modifier = Modifier.size(48.dp))
+            Spacer(Modifier.width(20.dp))
+            Column {
+                AppInfo(item, registrationState)
+                LastReceive(item)
+            }
+        }
+    }
+
+    private fun cacheIcon(context: Context, item: RegisteredApplication): BitmapPainter? {
+        if (!iconCache.containsKey(item.packageName)) {
+            val icon = getAppIcon(context, item)
+            iconCache[item.packageName] = icon
+            return icon
+        }
+        return null
+    }
+
+    @Composable
+    private fun LastReceive(item: RegisteredApplication) {
+        val context = LocalContext.current
         Text(
-            item.appName,
+            if (item.lastReceiveTime.time == 0L) ""
+            else stringResource(R.string.last_receive) + ParseUtils.getFriendlyDateString(
+                item.lastReceiveTime,
+                Utils.getUTC(),
+                context
+            ),
             style = MaterialTheme.typography.bodyLarge,
-            color = registrationState.second
-        )
-        Text(
-            registrationState.first,
-            style = MaterialTheme.typography.bodyMedium,
-            color = registrationState.second
         )
     }
-}
 
-@Composable
-private fun getIconFor(
-    item: RegisteredApplication
-): Painter {
-    if (LocalInspectionMode.current) {
-        return painterResource(android.R.mipmap.sym_def_app_icon)
-    }
-
-    val context = LocalContext.current
-    return BitmapPainter(item.getIcon(context).toBitmap().asImageBitmap())
-}
-
-@Preview(
-    showBackground = true,
-    device = Devices.PIXEL_3,
-    showSystemUi = true,
-)
-@Composable
-fun ApplicationListPreview() {
-    XLog.init()
-
-    ApplicationList {
-        val miPushApplications = ApplicationPageOperation.MiPushApplications()
-        miPushApplications.res = listOf(
-            registeredApplication(
-                RegisteredApplication.RegisteredType.NotRegistered,
-                "123"
-            ),
-            registeredApplication(
-                RegisteredApplication.RegisteredType.Registered,
-                "qwe"
-            ),
-            registeredApplication(
-                RegisteredApplication.RegisteredType.Registered,
-                "asd"
-            ),
-            registeredApplication(
-                RegisteredApplication.RegisteredType.Unregistered,
-                "zxc"
-            ),
-            registeredApplication(
-                RegisteredApplication.RegisteredType.Unregistered,
-                "456",
-                false
-            ),
-        ) + ('a'..'z').map {
-            registeredApplication(
-                RegisteredApplication.RegisteredType.NotRegistered,
-                it.toString()
+    @Composable
+    private fun AppInfo(
+        item: RegisteredApplication,
+        registrationState: Pair<String, Color>
+    ) {
+        Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+            Text(
+                item.appName,
+                style = MaterialTheme.typography.bodyLarge,
+                color = registrationState.second
+            )
+            Text(
+                registrationState.first,
+                style = MaterialTheme.typography.bodyMedium,
+                color = registrationState.second
             )
         }
-
-        miPushApplications
     }
-}
 
-@Preview(
-    showBackground = true,
-    device = Devices.PIXEL_3,
-    showSystemUi = true,
-)
-@Composable
-fun OneApplicationWithNonMiPushAppPreview() {
-    XLog.init()
-
-    ApplicationList {
-        val miPushApplications = ApplicationPageOperation.MiPushApplications()
-        miPushApplications.res = listOf(
-            registeredApplication(
-                RegisteredApplication.RegisteredType.NotRegistered,
-                "123"
-            )
-        )
-        miPushApplications.totalPkg = 100
-        miPushApplications
+    private val iconCache = HashMap<String, Painter>()
+    private fun getIconFromCache(
+        context: Context,
+        isPreview: Boolean,
+        item: RegisteredApplication
+    ): Painter {
+        return if (isPreview || !iconCache.containsKey(item.packageName))
+            getDefaultAppIcon(context)
+        else iconCache[item.packageName]!!
     }
-}
 
-private fun registeredApplication(
-    registeredType: Int,
-    appName: String,
-    existServices: Boolean = true
-): RegisteredApplication {
-    val registeredApplication = RegisteredApplication(
-        null,
-        appName,
-        RegisteredApplication.Type.ASK,
-        true,
-        false,
-        false,
-        false,
-        registeredType,
-        appName
+    private fun getAppIcon(context: Context, item: RegisteredApplication) =
+        BitmapPainter(item.getIcon(context).toBitmap().asImageBitmap())
+
+    private fun getDefaultAppIcon(context: Context): BitmapPainter {
+        if (!::defaultAppIcon.isInitialized) {
+            defaultAppIcon = BitmapPainter(
+                ImageBitmap.imageResource(context.resources, android.R.mipmap.sym_def_app_icon))
+        }
+        return defaultAppIcon
+    }
+
+    @Preview(
+        showBackground = true,
+        device = Devices.PIXEL_3,
+        showSystemUi = true,
     )
-    registeredApplication.existServices = existServices
-    return registeredApplication
+    @Composable
+    fun ApplicationListPreview() {
+        XLog.init()
+
+        ApplicationList {
+            val miPushApplications = ApplicationPageOperation.MiPushApplications()
+            miPushApplications.res = listOf(
+                registeredApplication(
+                    RegisteredApplication.RegisteredType.NotRegistered,
+                    "123"
+                ),
+                registeredApplication(
+                    RegisteredApplication.RegisteredType.Registered,
+                    "qwe"
+                ),
+                registeredApplication(
+                    RegisteredApplication.RegisteredType.Registered,
+                    "asd"
+                ),
+                registeredApplication(
+                    RegisteredApplication.RegisteredType.Unregistered,
+                    "zxc"
+                ),
+                registeredApplication(
+                    RegisteredApplication.RegisteredType.Unregistered,
+                    "456",
+                    false
+                ),
+            ) + ('a'..'z').map {
+                registeredApplication(
+                    RegisteredApplication.RegisteredType.NotRegistered,
+                    it.toString()
+                )
+            }
+
+            miPushApplications
+        }
+    }
+
+    @Preview(
+        showBackground = true,
+        device = Devices.PIXEL_3,
+        showSystemUi = true,
+    )
+    @Composable
+    fun OneApplicationWithNonMiPushAppPreview() {
+        XLog.init()
+
+        ApplicationList {
+            val miPushApplications = ApplicationPageOperation.MiPushApplications()
+            miPushApplications.res = listOf(
+                registeredApplication(
+                    RegisteredApplication.RegisteredType.NotRegistered,
+                    "123"
+                )
+            )
+            miPushApplications.totalPkg = 100
+            miPushApplications
+        }
+    }
+
+    private fun registeredApplication(
+        registeredType: Int,
+        appName: String,
+        existServices: Boolean = true
+    ): RegisteredApplication {
+        val registeredApplication = RegisteredApplication(
+            null,
+            appName,
+            RegisteredApplication.Type.ASK,
+            true,
+            false,
+            false,
+            false,
+            registeredType,
+            appName
+        )
+        registeredApplication.existServices = existServices
+        return registeredApplication
+    }
+
 }
