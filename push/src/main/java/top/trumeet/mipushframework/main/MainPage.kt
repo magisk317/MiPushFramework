377ec4e3de182eada2b5f556b9a1aa110a875526
Nihility <Nihility.t@gmail.com>
2025-03-23 15:49:46 +0800

    refactor: extract SearchBar to separate file

--- push/src/main/java/top/trumeet/mipushframework/main/MainPage.kt
+++ push/src/main/java/top/trumeet/mipushframework/main/MainPage.kt
@@ -6,40 +6,24 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Clear
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -51,10 +35,8 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.xiaomi.xmsf.R
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import top.trumeet.mipushframework.MainPageUtils
+import top.trumeet.mipushframework.component.SearchBar
 import top.trumeet.ui.theme.Theme
 
 private val mainPageUtils1 = MainPageUtils()
@@ -212,52 +194,6 @@ private fun MainSettingsPreview() {
         }
     }
 }
-
-@Composable
-@OptIn(ExperimentalMaterial3Api::class)
-private fun SearchBar(placeholder: String, onValueChange: (String) -> Unit) {
-    val focusManager = LocalFocusManager.current
-    var query by rememberSaveable { mutableStateOf("") }
-    val ph by rememberUpdatedState(placeholder)
-    val debounceOnValueChange: (String) -> Unit = debounce(onValueChange)
-    val change: (String) -> Unit = { query = it; debounceOnValueChange(it) }
-    TopAppBar(title = {
-        TextField(
-            value = query,
-            onValueChange = change,
-            modifier = Modifier.fillMaxWidth(),
-            placeholder = { Text(ph) },
-            trailingIcon = {
-                if (query.isNotEmpty()) {
-                    IconButton({ change("") }) {
-                        Icon(Icons.Default.Clear, contentDescription = "Clear")
-                    }
-                }
-            },
-            singleLine = true,
-            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
-            keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus() }),
-            colors = TextFieldDefaults.textFieldColors(containerColor = Color.Transparent)
-        )
-    })
-}
-
-@Composable
-private fun debounce(onValueChange: (String) -> Unit): (String) -> Unit {
-    val scope = rememberCoroutineScope()
-    var job: Job? = null
-    val change: (String) -> Unit = {
-        scope.launch {
-            job?.cancel()
-            job = launch {
-                delay(300)
-                onValueChange(it)
-            }
-        }
-    }
-    return change
-}
-
 
 @Preview(
     showBackground = true,
