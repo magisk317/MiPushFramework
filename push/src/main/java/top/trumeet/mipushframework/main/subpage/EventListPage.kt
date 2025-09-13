46e396e7b81c546cf61c1112bf19ed75a227c663
Nihility <Nihility.t@gmail.com>
2025-09-13 16:16:20 +0800

    feat: limit the height of event info

--- push/src/main/java/top/trumeet/mipushframework/main/subpage/EventListPage.kt
+++ push/src/main/java/top/trumeet/mipushframework/main/subpage/EventListPage.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -26,12 +27,15 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.DialogProperties
 import com.elvishew.xlog.XLog
 import com.xiaomi.xmsf.R
 import com.xiaomi.xmsf.push.utils.RegSecUtils
@@ -121,6 +125,10 @@ private fun EventDetailsDialog(
         )
     }
     val context = LocalContext.current
+
+    val screenHeight = LocalConfiguration.current.screenHeightDp.dp
+    val targetHeight = screenHeight * 0.9f
+
     AlertDialog(
         onDismiss,
         {
@@ -169,7 +177,8 @@ private fun EventDetailsDialog(
         },
         text = {
             TextView(json)
-        }
+        },
+        modifier = Modifier.heightIn(Dp.Unspecified, targetHeight)
     )
 }
 
