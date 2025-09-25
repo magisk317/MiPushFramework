ac6078a5d57ccc672b7237f02c9efc7c579b494f
Nihility <Nihility.t@gmail.com>
2025-09-25 14:17:21 +0800

    feat: add crash log file

--- push/src/main/java/com/xiaomi/xmsf/CrashHandler.java
+++ push/src/main/java/com/xiaomi/xmsf/CrashHandler.java
@@ -4,6 +4,12 @@ import android.widget.Toast;
 
 import com.elvishew.xlog.Logger;
 import com.elvishew.xlog.XLog;
+import com.elvishew.xlog.flattener.ClassicFlattener;
+import com.elvishew.xlog.printer.file.FilePrinter;
+import com.elvishew.xlog.printer.file.backup.NeverBackupStrategy;
+import com.elvishew.xlog.printer.file.clean.FileLastModifiedCleanStrategy;
+import com.elvishew.xlog.printer.file.naming.DateFileNameGenerator;
+import com.xiaomi.xmsf.utils.LogUtils;
 
 import top.trumeet.common.utils.Utils;
 
@@ -17,6 +23,7 @@ public class CrashHandler {
     public static void installCrashLogger() {
         final String TAG = CrashHandler.class.getSimpleName();
         final Logger logger = XLog.tag(TAG).build();
+        final Logger crashLogger = XLog.tag(TAG).printers(createCrashPrinter()).build();
 
         install((t, e) -> {
             StringBuilder crashInfo = new StringBuilder();
@@ -29,7 +36,23 @@ public class CrashHandler {
             }
             Utils.makeText(crashInfo, Toast.LENGTH_LONG);
             logger.e("Mi Push Crash", e);
+            crashLogger.e("Mi Push Crash", e);
         });
+    }
+
+    private static FilePrinter createCrashPrinter() {
+        int _7DaysInMillis = 7 * 24 * 60 * 60 * 1000;
+        return new FilePrinter.Builder(LogUtils.getLogFolder(Utils.getApplication()))
+                .fileNameGenerator(new DateFileNameGenerator() {
+                    @Override
+                    public String generateFileName(int logLevel, long timestamp) {
+                        return "Crash_" + super.generateFileName(logLevel, timestamp);
+                    }
+                })
+                .backupStrategy(new NeverBackupStrategy())
+                .cleanStrategy(new FileLastModifiedCleanStrategy(_7DaysInMillis))
+                .flattener(new ClassicFlattener())
+                .build();
     }
 
     public static void install(Thread.UncaughtExceptionHandler handler) {
