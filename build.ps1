4bd8e3a6d04c98e87ffee8e72eed004577d365a8
Nihility <Nihility.t@gmail.com>
2025-09-28 18:06:38 +0800

    ci: speed up build script

--- build.ps1
+++ build.ps1
@@ -1,24 +1,43 @@
+
+function build {
+
 $gradleTasks = @(
+	,@(
 # build apks
-	"build",
+		,"build"
 
 # run unit tests
-	"test",
+		,"test"
+	)
 
+	,@(
 # run android tests
-	":common:connectedAndroidTest",
-	":mipush_hook:connectedAndroidTest",
-	#":push:connectedVc105DebugAndroidTest",
-	":push:connectedNormalDebugAndroidTest"
+		,":common:connectedAndroidTest"
+		,":mipush_hook:connectedAndroidTest"
+		#,":push:connectedVc105DebugAndroidTest",
+		,":push:connectedNormalDebugAndroidTest"
+	)
 )
 
 $width = $Host.UI.RawUI.WindowSize.Width
 foreach ($task in $gradleTasks) {
-	./gradlew ($task -split ' +')
+	$task = @() + $task
+	Write-Host
+	Write-Host
+	Write-Host "Run Tasks: $((@('') + $task) -join "`n    ")" -ForegroundColor Green
+	./gradlew @task
 	if ($LASTEXITCODE -ne 0) {
-		Write-Host "task failed: $task".PadRight($width) -BackgroundColor Red -ForegroundColor Black
+		Write-Host "BUILD FAILED".PadRight($width) -BackgroundColor Red -ForegroundColor White
 		return
 	}
 }
 
-Write-Host "all tasks success!".PadRight($width) -BackgroundColor Green -ForegroundColor Black
+Write-Host "BUILD SUCCESSFUL".PadRight($width) -BackgroundColor Green -ForegroundColor Black
+
+}
+
+
+$elapsed = Measure-Command { build | Out-Default }
+
+Write-Host -ForegroundColor Cyan ("{0} days {1:d2}:{2:d2}:{3:d2}:{4:d3}" -f
+		$elapsed.Days, $elapsed.Hours, $elapsed.Minutes, $elapsed.Seconds, $elapsed.Milliseconds)
