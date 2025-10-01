db69b15f277fc2ac1c821399f1377fb13541c0b6
Nihility <Nihility.t@gmail.com>
2025-10-01 11:43:56 +0800

    feat: log event flow

--- push/src/main/java/com/nihility/MiPushEventListener.java
+++ push/src/main/java/com/nihility/MiPushEventListener.java
@@ -2,9 +2,15 @@ package com.nihility;
 
 import android.content.Intent;
 
+import com.elvishew.xlog.Logger;
+import com.elvishew.xlog.XLog;
 import com.xiaomi.xmpush.thrift.XmPushActionContainer;
+import com.xiaomi.xmsf.utils.ConvertUtils;
 
 public class MiPushEventListener {
+    private static final String TAG = MiPushEventListener.class.getSimpleName();
+    private static final Logger logger = XLog.tag(TAG).build();
+
     private static class LazyHolder {
         volatile static MiPushEventListener INSTANCE = new MiPushEventListener();
     }
@@ -20,16 +26,19 @@ public class MiPushEventListener {
     }
 
     public void receiveFromServer(XmPushActionContainer container) {
-
+        logger.i("From Server     : " + ConvertUtils.toJson(container));
     }
 
     public void transferToApplication(XmPushActionContainer container) {
+        logger.i("To   Application: " + ConvertUtils.toJson(container));
     }
 
     public void receiveFromApplication(Intent intent) {
+        logger.i("From Application: " + ConvertUtils.toJson(intent));
     }
 
     public void transferToServer(Intent intent) {
+        logger.i("To   Server     : " + ConvertUtils.toJson(intent));
     }
 
 }
