8ef2af8f0315a343e4d8bd158d8c99c71cd6fc94
magisk317 <code@mail.usdt.edu.kg>
2026-05-06 12:09:11 +0800

    ci(deps): migrate to self-hosted renovate and remove dependabot workflows

---
+++ .github/renovate-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  platform: "github",
+  repositories: ["magisk317/MiPushFramework"],
+  onboarding: false,
+  requireConfig: "required",
+  allowedUnsafeExecutions: ["gradleWrapper"],
+};
