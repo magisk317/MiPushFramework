67deb22b68c2b01ee6c3d2db8777e90b1b5c6590
magisk317 <93979778+magisk317@users.noreply.github.com>
2026-06-26 01:13:27 +0800

    refactor(settings): remove mock notification entry

--- manager/src/main/java/io/github/magisk317/mipush/main/viewmodel/AdvancedSettingsViewModel.kt
+++ manager/src/main/java/io/github/magisk317/mipush/main/viewmodel/AdvancedSettingsViewModel.kt
@@ -73,19 +73,7 @@ class AdvancedSettingsViewModel constructor(
     fun startMiPushServiceAsForegroundService(context: android.content.Context) {
         settingsManager.startMiPushServiceAsForegroundService(context)
     }
-    
-    fun notifyMockNotification(context: android.content.Context) {
-        settingsManager.notifyMockNotification(context)
-    }
 
-    fun notifyMockNotification(
-        context: android.content.Context,
-        kind: io.github.magisk317.mipush.feature.diagnostic.MockNotificationKind,
-        packageName: String
-    ) {
-        settingsManager.notifyMockNotification(context, kind, packageName)
-    }
-    
     fun clearHistory(context: android.content.Context) {
         settingsManager.clearHistory(context, viewModelScope)
     }
